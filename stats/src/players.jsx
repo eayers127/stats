f894a2634198fce8843c9c0b283aa6125379d959
Ethan Ayers <ethan.ayers88@gmail.com>
2023-04-27 18:47:57 -0600

    Added search option

--- stats/src/players.jsx
+++ stats/src/players.jsx
@@ -4,6 +4,7 @@ import { API_TOKEN } from './config';
 function Players() {
   const [players, setPlayers] = useState([]);
   const [selectedLeague, setSelectedLeague] = useState('PL');
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch(`http://api.football-data.org/v2/competitions/${selectedLeague}/scorers`, {
@@ -11,25 +12,49 @@ function Players() {
         'X-Auth-Token': API_TOKEN
       }
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setPlayers(data.scorers);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // log the response data to the console
+        setPlayers(data.scorers); // set the state with the player data
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   }, [selectedLeague]);
+
+  const handleLeagueChange = (event) => {
+    setSelectedLeague(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPlayers = players.filter(player =>
+    player.player.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      <h2>Top Scorers in the {selectedLeague === 'PL' ? 'Premier League' : 'La Liga'}</h2> {/* conditional rendering for league title */}
+      <h2>Top Scorers</h2>
       <div>
-        <button onClick={() => setSelectedLeague('PL')}>Premier League</button> {/* button to select PL */}
-        <button onClick={() => setSelectedLeague('PD')}>La Liga</button> {/* button to select La Liga */}
+        <label htmlFor="league-select">Select a league:</label>
+        <select id="league-select" value={selectedLeague} onChange={handleLeagueChange}>
+          <option value="PL">Premier League</option>
+          <option value="PD">La Liga</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="search-input">Search for a player:</label>
+        <input
+          type="text"
+          id="search-input"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
       </div>
       <ul>
-        {players.map(player => (
+        {filteredPlayers.map(player => (
           <li key={player.player.id}>
             {player.player.name} ({player.team.name}): {player.numberOfGoals} goals
           </li>
